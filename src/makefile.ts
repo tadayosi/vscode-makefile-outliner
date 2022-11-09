54470bb1018fa18da42fad7752f27945d4a32038
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2022-11-09 23:11:36 +0900

    feat: support variables and functions

--- src/makefile.ts
+++ src/makefile.ts
@@ -7,32 +7,94 @@ class MakefileSymbolProvider implements vscode.DocumentSymbolProvider {
     '.SILENT', '.EXPORT_ALL_VARIABLES', '.NOTPARALLEL', '.ONESHELL', '.POSIX',
   ];
   private readonly targetPattern = /^([^#\s]+)\s*:[^=]*$/;
+  private readonly variablePattern = /^([^:#=\s]+)\s*(=|:=|::=|:::=|[?]=).*$/;
+  private readonly functionPattern = /^define ([^:#=\s]+)\s*$/;
 
   provideDocumentSymbols(document: vscode.TextDocument, token: vscode.CancellationToken)
     : Thenable<vscode.SymbolInformation[]> {
     return new Promise((resolve, reject) => {
-      const symbols = [];
+      const symbols: vscode.SymbolInformation[] = [];
 
       for (let i = 0; i < document.lineCount; i++) {
         const line = document.lineAt(i);
-        const match = line.text.match(this.targetPattern);
-        if (match) {
-          const target = match[1];
-          // exclude special targets from outline
-          if (this.specialTargets.includes(target)) {
-            continue;
-          }
-          symbols.push(new vscode.SymbolInformation(
-            target,
-            vscode.SymbolKind.Field,
-            '',
-            new vscode.Location(document.uri, line.range)
-          ));
+        if (this.matchTarget(document, line, symbols)) {
+          continue;
         }
+        if (this.matchVariable(document, line, symbols)) {
+          continue;
+        }
+        this.matchFunction(document, line, symbols);
       }
 
       resolve(symbols);
     });
+  }
+
+  private matchTarget(
+    document: vscode.TextDocument,
+    line: vscode.TextLine,
+    symbols: vscode.SymbolInformation[],
+  ): boolean {
+    const match = line.text.match(this.targetPattern);
+    if (!match) {
+      return false;
+    }
+
+    const target = match[1];
+    // exclude special targets from outline
+    if (this.specialTargets.includes(target)) {
+      return true;
+    }
+
+    symbols.push(new vscode.SymbolInformation(
+      target,
+      vscode.SymbolKind.Field,
+      '',
+      new vscode.Location(document.uri, line.range)
+    ));
+    return true;
+  }
+
+  private matchVariable(
+    document: vscode.TextDocument,
+    line: vscode.TextLine,
+    symbols: vscode.SymbolInformation[],
+  ): boolean {
+    const match = line.text.match(this.variablePattern);
+    if (!match) {
+      return false;
+    }
+
+    const variable = match[1];
+
+    symbols.push(new vscode.SymbolInformation(
+      variable,
+      vscode.SymbolKind.Variable,
+      '',
+      new vscode.Location(document.uri, line.range)
+    ));
+    return true;
+  }
+
+  private matchFunction(
+    document: vscode.TextDocument,
+    line: vscode.TextLine,
+    symbols: vscode.SymbolInformation[],
+  ): boolean {
+    const match = line.text.match(this.functionPattern);
+    if (!match) {
+      return false;
+    }
+
+    const func = match[1];
+
+    symbols.push(new vscode.SymbolInformation(
+      func,
+      vscode.SymbolKind.Function,
+      '',
+      new vscode.Location(document.uri, line.range)
+    ));
+    return true;
   }
 }
 
